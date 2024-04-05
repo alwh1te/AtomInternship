31bbf015be6056d8facab4f7e762fd3e8d64e4d9
alw <alexander.s.filatov@gmail.com>
2024-04-05 15:49:32 +0300

    working get, post and put

---
+++ src/test/java/org/example/atom/AtomApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.atom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AtomApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
